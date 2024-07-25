d51b8077dfce08b6cc0453f426a343ee9ef3b307
samirulcodes <samirulislam.official.com>
2024-07-25 13:22:28 +0530

    App jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,13 +2,13 @@
 import Chai from "./chai"
 
 function App() {
- 
+  const username="chai aur code"
 
   return (
     // jsx allow only one one element so we make a div element and wrap other tag in div tag
     <div>
     <Chai/>
-    <h1>chai aur react</h1>
+    <h1>chai aur react {username}</h1>
     <p>test para</p>
     </div>
   )
