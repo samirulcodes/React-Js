8694cf1b01cf2e4a7ca936f2f5c840f8f658d733
samirulcodes <samirulislam.official.com>
2024-07-29 22:22:33 +0530

    tailwindProps

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Card from './components/Card'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import Card from './components/Card'
 
-function App() {
-  const [count, setCount] = useState(0)
-  let myObj={
-    name:"aman",
-    age:20
-  }
+// function App() {
+//   const [count, setCount] = useState(0)
+//   let myObj={
+//     name:"aman",
+//     age:20
+//   }
 
-  let myArr=[1,2,3]
+//   let myArr=[1,2,3]
 
-  return (
-    <>
-     <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
-    {/* <Card channel="react" obj={myObj} Arr={myArr} /> */}
+//   return (
+//     <>
+//      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+//     {/* <Card channel="react" obj={myObj} Arr={myArr} /> */}
 
-    <Card username="react" btn="Click me />
-    <Card username="aman" btn="Visit me" />
-    </>
-  )
-}
+//     <Card username="react" btn="Click me />
+//     <Card username="aman" btn="Visit me" />
+//     </>
+//   )
+// }
 
-export default App
+// export default App
