fb03942b0e25f1c7c2ab0c9b90d4e932912cb89b
samirulcodes <samirulislam.official.com>
2024-08-02 14:37:58 +0530

    react router

---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+
+// creating Router
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
