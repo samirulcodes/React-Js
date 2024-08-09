0230ac0452ceebf5f167f8ea39fe2975d8a92531
samirulcodes <samirulislam.official.com>
2024-08-10 02:32:09 +0530

    mega blog

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  // taking access of env 
+    console.log(import.meta.env.VITE_APPWRITE_URL);
+      
+
+  return (
+    <>
+      <h1>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,15 @@
+const conf = {
+    // benifits -> we take from appwriteUrl (or other variable stated below) from conf and the value we get always a String , it will be be empty string but it will be String
+
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+// there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
+
+
+
+
+export default conf
